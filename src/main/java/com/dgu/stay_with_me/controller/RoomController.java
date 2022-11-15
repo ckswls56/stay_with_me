ab0315b5849d9d2432cc36a8942b4cc8d907b433
Nachanjin <skxkswls@gmail.com>
2022-11-15 22:43:32 +0900

    Room END

--- src/main/java/com/dgu/stay_with_me/controller/RoomController.java
+++ src/main/java/com/dgu/stay_with_me/controller/RoomController.java
@@ -9,12 +9,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Optional;
 
+@Controller
 @RequestMapping("room")
 public class RoomController {
 
@@ -24,15 +27,44 @@ public class RoomController {
     RoomService roomService;
 
     //모든 방 조회
-
     @GetMapping()
     public ResponseEntity<List<Room>> getAllRooms(){
         List<Room> rooms = roomService.findAll();
         return new ResponseEntity<List<Room>>(rooms, HttpStatus.OK);
     }
+    
+    // roomId로 방하나 조회
+    @GetMapping(value = "/{roomId}")
+    public ResponseEntity<Room> getRoom(@PathVariable("roomId")Integer roomId){
+        Optional<Room> room = roomService.findById(roomId);
+        return new ResponseEntity<Room>(room.get(),HttpStatus.OK);
+    }
+
+    // roomId로 방 삭제
+    @DeleteMapping(value = "/{roomId}")
+    public ResponseEntity<Room> deleteRoom(@PathVariable("roomId") Integer roomId){
+        roomService.deleteById(roomId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // roomId로 방 수정
+
+    @PutMapping(value = {"/{roomId}"})
+    public ResponseEntity<Room> updateRoom(@PathVariable("roomId") Integer roomId,Room room){
+        roomService.updateById(roomId,room);
+        return new ResponseEntity<Room>(room,HttpStatus.OK);
+    }
+
+    //방 입력
     @PostMapping
     public ResponseEntity<Room> save(Room room){
         return new ResponseEntity<Room>(roomService.save(room),HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/saveRoom",method = RequestMethod.GET)
+    public ResponseEntity<Room> save(HttpServletRequest req, Room room){
+        return new ResponseEntity<Room>(roomService.save(room),HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/com/dgu/stay_with_me/repository/RoomRepository.java
+++ src/main/java/com/dgu/stay_with_me/repository/RoomRepository.java
@@ -2,9 +2,10 @@ package com.dgu.stay_with_me.repository;
 
 import com.dgu.stay_with_me.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
 
     public List<Room> findByRoomId(int id);
