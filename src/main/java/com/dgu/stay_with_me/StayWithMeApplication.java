bd2df68c433f6dd6e096663730ae5609c089a7b2
Nachanjin <skxkswls@gmail.com>
2022-11-15 14:18:19 +0900

    Initial commit

---
+++ src/main/java/com/dgu/stay_with_me/StayWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.dgu.stay_with_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgu/stay_with_me/StayWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgu.stay_with_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
